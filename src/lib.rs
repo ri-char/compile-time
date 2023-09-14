3e8c3c128d94d60a43128e63046b23a578eaa535
riChar <wxsychi@163.com>
2023-09-14 11:16:17 +0800

    format timestamp

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,41 @@
 use std::time::UNIX_EPOCH;
 
-use proc_macro::TokenStream;
-use quote::quote;
+use chrono::Local;
+use proc_macro::{TokenStream, TokenTree};
+use syn::{parse::Parse, parse_macro_input, LitStr};
 
 extern crate proc_macro;
 
+struct ArgsMacroInfo {
+    format: Option<String>,
+}
+
+impl Parse for ArgsMacroInfo {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        if input.is_empty() {
+            return Ok(ArgsMacroInfo { format: None });
+        }
+        let format: LitStr = input.parse()?;
+        return Ok(ArgsMacroInfo {
+            format: Some(format.value()),
+        });
+    }
+}
+
 #[proc_macro]
-pub fn compile_time(_input: TokenStream) -> TokenStream {
-    let time = std::time::SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis();
-    TokenStream::from(quote! {#time})
+pub fn compile_time(input: TokenStream) -> TokenStream {
+    let ArgsMacroInfo { format } = parse_macro_input!(input as ArgsMacroInfo);
+    match format {
+        None => {
+            let time = std::time::SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_millis();
+            TokenTree::Literal(proc_macro::Literal::u128_suffixed(time)).into()
+        }
+        Some(format) => {
+            let time = Local::now().format(&format).to_string();
+            TokenTree::Literal(proc_macro::Literal::string(&time)).into()
+        }
+    }
 }
